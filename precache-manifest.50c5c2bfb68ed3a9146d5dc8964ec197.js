2de395a66eff124a0d65a5a71954c57ee7da4136
Vasiliy Rusin <RusinVA@gmail.com>
2022-05-15 08:21:16 +0000

    deploy

---
+++ precache-manifest.50c5c2bfb68ed3a9146d5dc8964ec197.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "864d6c723e745fa5aa02",
+    "url": "/novosibirsk-city-transport/css/app.d8dcb040.css"
+  },
+  {
+    "revision": "e3c83e937c04ebdfc7d4",
+    "url": "/novosibirsk-city-transport/css/chunk-vendors.f32eaac2.css"
+  },
+  {
+    "revision": "0b27fb17412d5bb3b26239cac1546a2f",
+    "url": "/novosibirsk-city-transport/index.html"
+  },
+  {
+    "revision": "864d6c723e745fa5aa02",
+    "url": "/novosibirsk-city-transport/js/app.e3796196.js"
+  },
+  {
+    "revision": "e3c83e937c04ebdfc7d4",
+    "url": "/novosibirsk-city-transport/js/chunk-vendors.6c09a42a.js"
+  },
+  {
+    "revision": "c534fa0e4e92c93797d0926632c654e0",
+    "url": "/novosibirsk-city-transport/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/novosibirsk-city-transport/robots.txt"
+  }
+]);
